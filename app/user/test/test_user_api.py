bffecc95c8799dbd2f35a49487b4f0b301235063
starscream2290 <cayabyabcristopher@gmail.com>
2021-08-07 17:32:26 +0800

    Added Manage User Endpoints.

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -7,6 +7,8 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
+
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -87,3 +89,48 @@ class PublicUserAPITests(TestCase):
 
         self.assertNotIn('token', responce.data)
         self.assertEqual(responce.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        "Test that authenticaton is required for users"
+        responce = self.client.get(ME_URL)
+
+        self.assertEqual(responce.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateUserApiTests(TestCase):
+    "Test API request that require authenticaton"
+
+    def setUp(self):
+        self.user = create_user(
+            email = 'test@pldt.com',
+            password = 'testpw123',
+            name = 'name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        "Test retrieveing profile for logged in used"
+        responce = self.client.get(ME_URL)
+
+        self.assertEqual(responce.status_code, status.HTTP_200_OK)
+        self.assertEqual(responce.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        "test that POST is not allowed on the ME URL"
+        responce = self.client.post(ME_URL, {})
+
+        self.assertEqual(responce.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        "Test updating the user profile for authenticated user"
+        payload = {'name':'new name', 'password':'newpass123'}
+
+        responce = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(responce.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -12,3 +12,13 @@ class CreateTokenView(ObtainAuthToken):
     "Create a new auth tokenm for user"
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    "Manage the Authenticated User"
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        "Retrieve and Return authentication user"
+        return self.request.user
